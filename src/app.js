4a63b8f4d3c89eef3ef2b914c7a5ddf0c37c498f
Harsh Kumar <harsh@notyouridea.com>
2025-05-05 00:08:00 +0530

    done till data santization and schema validation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/database");
+var validator = require('validator');
 const app = express();
 const User = require("./models/user")
-app.get("/user", (req, res) => {
+app.get("/useriid", (req, res) => {
     console.log(req.query)
     res.send(
         {
@@ -20,7 +21,7 @@ app.get("/hi", (req, res, next) => {
     res.send("Second route handler") //will not execute as soon as it sends first response
     console.log("2nd response")
 })
-app.get("/user/:id", (req, res) => {
+app.get("/use/:id", (req, res) => {
     console.log(req.params.id)
     res.send(
         {
@@ -49,7 +50,7 @@ app.use("/", (err, req, res, next) => {
 app.get("/hello", (req, res) => {
     res.send("Hello G. from /hello")
 })
-app.use(express.json());
+app.use(express.json()); //change json to object
 app.post("/signup", async (req, res) => {
     const userObj = {
         firstName: "Harsh",
@@ -67,6 +68,11 @@ app.post("/signup", async (req, res) => {
     //     _id: "666666634423225525533"
     // });
     console.log(req.body)
+    const { emailId } = req.body
+    if (!req.body.lastName) {
+        res.status(400).send("Please add last name");
+        return;
+    }
     const user = new User(req.body);
     try {
         await user.save();
@@ -76,6 +82,125 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+//get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "User data fetched successfully",
+                data: users
+            });
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+//get one user by email
+app.get("/one-user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        console.log(userEmail)
+        const user = await User.findOne({ emailId: userEmail }); //find one return object or null //if dont pass anything it will giev the first document.
+        if (!user) {
+            res.status(404).send("User not found");
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "User data fetched successfully",
+                data: user
+            });
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+// get all users
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({
+            success: true,
+            message: "User data fetched successfully",
+            users: users
+        });
+    } catch (error) {
+        res.status(400).send("Something went wrong...")
+    }
+})
+
+//delete one user
+app.delete("/delete-user/:id", async (req, res) => {
+    const userId = req.params.id
+    console.log(userId)
+    try {
+        // const deletedUser = await User.findByIdAndDelete({ _id: userId })
+        const deletedUser = await User.findByIdAndDelete(userId)
+        console.log(deletedUser)
+        if (!deletedUser) {
+            res.status(404).send("User Not Found..")
+        }
+        res.status(200).send("User deleted successfully.")
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(400).send("Something went wrong...")
+    }
+})
+
+//update data of one user
+app.patch("/update-user/:id", async (req, res) => {
+    const updateUserId = req.params.id
+    const data = req.body;
+    try {
+        if (!data) {
+            res.status(404).send("No data found....")
+        }
+        const allowedUpdates = ["age", "emailId", "password"]
+        const isUpdateAllowed = Object.keys(data).every((k) => {
+            return allowedUpdates.includes(k)
+        })
+        console.log("Incoming update data:", data);
+        if (!isUpdateAllowed) {
+            // res.status(400).send("Update not allowed..")
+            // return
+
+            throw new Error("Update Not allowed....")
+        }
+        // if (!validator.isEmail(req.body.emailId)) {
+        //     res.status(400).send("Email is not valid...")
+        //     return
+        // }
+        const updatedUserId = await User.findByIdAndUpdate(updateUserId, data, { returnDocument: "after", runValidators: true }) //after will return user after the update //default value is before.. //run schema validators in update as well.
+        res.status(200).send(`User ${updatedUserId.firstName} updated successfully....`)
+        console.log(updatedUserId.firstName)
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(400).send("Something went wrong...")
+    }
+})
+//by email
+app.patch("/updat-user/:email", async (req, res) => {
+    const updateEmail = req.params.email
+    const data = req.body;
+    console.log("By Email...")
+    try {
+        if (!data) {
+            res.status(404).send("No data found....")
+        }
+        const updatedUserId = await User.findOneAndUpdate({ emailId: updateEmail }, data, { new: true }) //after will return user after the update //default value is before.. //{ new: true } do the same work as return after
+        res.status(200).send(`User ${updatedUserId.firstName} updated successfully....`)
+        console.log(updatedUserId.firstName)
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(400).send("Something went wrong...")
+    }
+})
 connectDB().then(() => {
     console.log("Database connection established...")
     app.listen(7777, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,57 @@
 const mongoose = require("mongoose");
-
+var validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true, //if required true and default is there. even if firstName not there it will store defult name
+        default: "No name",
+        minLength: 4
     },
     lastName: {
-        type: String
+        type: String,
+        lowercase: true,
     },
     emailId: {
-        type: String
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email Not Valid")
+            }
+        }
     },
     password: {
         type: String
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 60
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender Not Valid");
+            }
+        }
     },
-    _id: {
-        type: Number
+    photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
+    },
+    about: {
+        type: String,
+        default: "This is the default description about the user"
+    },
+    skills: {
+        type: [String]
     }
+}, {
+    timestamps: true  // Automatically adds createdAt and updatedAt
 });
 
 const User = mongoose.model("User", userSchema);
